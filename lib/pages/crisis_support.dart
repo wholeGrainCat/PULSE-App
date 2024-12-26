a5062daf57aa9aa0e7c8cfb21040a97645fd7a99
tannnnnnnnnnnqqq <xueqian0731@gmail.com>
2024-12-26 12:24:39 +0800

    update build gradle

--- lib/pages/crisis_support.dart
+++ lib/pages/crisis_support.dart
@@ -1,19 +1,267 @@
 import 'package:flutter/material.dart';
+import 'package:student/pages/resource_library.dart';
+import 'package:student/pages/mood_tracker.dart';
+import 'package:student/pages/student_dashboard.dart';
+import 'package:student/components/bottom_navigation.dart'; // Import your BottomNavigation widget
+
+void main() {
+  runApp(const CrisisSupport());
+}
 
 class CrisisSupport extends StatelessWidget {
   const CrisisSupport({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const CrisisSupportPage(),
+      routes: {
+        '/resource': (context) => const ResourceLibraryPage(),
+        '/moodtracker': (context) => const MoodTrackerPage(),
+        '/studentdashboard': (context) => const StudentDashboard(),
+        //'/chat': (context) => const ChatScreen(),
+        //'/profile': (context) => const ProfileScreen(),
+      },
+    );
+  }
+}
+
+class CrisisSupportPage extends StatefulWidget {
+  const CrisisSupportPage({super.key});
+
+  @override
+  _CrisisSupportPageState createState() => _CrisisSupportPageState();
+}
+
+class _CrisisSupportPageState extends State<CrisisSupportPage> {
+  int _currentIndex = 0;
+
+  void _onTap(int index) {
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/resource');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/moodtracker');
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/studentdashboard');
+        break;
+      case 3:
+        Navigator.pushNamed(context, '/chat');
+        break;
+      case 4:
+        Navigator.pushNamed(context, '/profile');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Crisis Support'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
-            Navigator.pop(
-                context); // This will navigate back to the previous screen
+            Navigator.pushNamed(context, '/studentdashboard');
           },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFA4E3E8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Are You Feeling Suicidal?',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image.asset(
+                      'assets/images/suicidal_feeling.png',
+                      width: 60,
+                      height: 60,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: const [
+                MentalHealthHotline(),
+                EmergencyHotline(),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigation(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          _onTap(index);
+        },
+      ),
+    );
+  }
+}
+
+class MentalHealthHotline extends StatelessWidget {
+  const MentalHealthHotline({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Mental Health Hotline',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          HotlineButton(
+            title: 'Befrienders 082-242800',
+            icon: Icons.phone,
+            iconColor: Colors.black,
+            backgroundColor: Color(0xFFD9F65C),
+            textColor: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EmergencyHotline extends StatelessWidget {
+  const EmergencyHotline({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Emergency Contact Hotline',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          HotlineButton(
+            title: 'Polis Bantuan UNIMAS +6017-212 7464',
+            icon: Icons.local_police,
+            backgroundColor: Colors.red,
+          ),
+          HotlineButton(
+            title: 'Rescue 991',
+            icon: Icons.fire_truck,
+            backgroundColor: Colors.red,
+          ),
+          HotlineButton(
+            title: 'Hotline 082-244444',
+            icon: Icons.support_agent,
+            backgroundColor: Colors.red,
+          ),
+          HotlineButton(
+            title: '082-230689',
+            icon: Icons.local_hospital,
+            backgroundColor: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HotlineButton extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Color backgroundColor;
+  final Color textColor;
+  final Color iconColor;
+
+  const HotlineButton({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.backgroundColor,
+    this.textColor = Colors.white,
+    this.iconColor = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+        ),
+        onPressed: () {
+          // Add functionality here
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color: iconColor,
+            ),
+            const SizedBox(width: 16),
+            Text(
+              title,
+              style: TextStyle(
+                color: textColor,
+                fontSize: 16,
+              ),
+            ),
+          ],
         ),
       ),
     );
