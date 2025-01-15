9b7e03fe5126c097370493b6b3a3d095d2b8df6b
tannnnnnnnnnnqqq <xueqian0731@gmail.com>
2025-01-15 22:52:05 +0800

    update change password for admin and student

--- lib/Admin/profile/change_password_page.dart
+++ lib/Admin/profile/change_password_page.dart
@@ -1,143 +1,230 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ChangePasswordPage extends StatelessWidget {
+class ChangePasswordPage extends StatefulWidget {
   const ChangePasswordPage({super.key});
 
   @override
+  _ChangePasswordPageState createState() => _ChangePasswordPageState();
+}
+
+class _ChangePasswordPageState extends State<ChangePasswordPage> {
+  final currentPasswordController = TextEditingController();
+  final newPasswordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  // Password visibility states
+  bool isCurrentPasswordVisible = false;
+  bool isNewPasswordVisible = false;
+  bool isConfirmPasswordVisible = false;
+
+  @override
   Widget build(BuildContext context) {
-    // TextEditingControllers for input fields
-    final currentPasswordController = TextEditingController();
-    final newPasswordController = TextEditingController();
-    final confirmPasswordController = TextEditingController();
-
     return Scaffold(
-      body: Column(
-        children: [
-          // App Bar
-          AppBar(
-            title: const Text(
-              'Change Password',
-              style: TextStyle(color: Colors.black),
-            ),
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            centerTitle: true,
-            iconTheme: const IconThemeData(color: Colors.black),
-          ),
+      appBar: AppBar(
+        title: const Text(
+          'Change Password',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 20),
 
-          // Page Content
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(height: 20), // Add spacing below AppBar
-
-                // Current Password Input
-                TextField(
-                  controller: currentPasswordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Current Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    prefixIcon: const Icon(Icons.lock),
-                  ),
+            // Current Password Input
+            TextField(
+              controller: currentPasswordController,
+              obscureText: !isCurrentPasswordVisible,
+              decoration: InputDecoration(
+                labelText: 'Current Password',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                const SizedBox(height: 20),
-
-                // New Password Input
-                TextField(
-                  controller: newPasswordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'New Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    prefixIcon: const Icon(Icons.lock_outline),
+                prefixIcon: const Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isCurrentPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
                   ),
-                ),
-                const SizedBox(height: 20),
-
-                // Confirm New Password Input
-                TextField(
-                  controller: confirmPasswordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Confirm New Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    prefixIcon: const Icon(Icons.check_circle_outline),
-                  ),
-                ),
-                const SizedBox(height: 30),
-
-                // Change Password Button
-                ElevatedButton(
                   onPressed: () {
-                    final currentPassword = currentPasswordController.text;
-                    final newPassword = newPasswordController.text;
-                    final confirmPassword = confirmPasswordController.text;
-
-                    // Basic Validation
-                    if (currentPassword.isEmpty ||
-                        newPassword.isEmpty ||
-                        confirmPassword.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Please fill in all fields'),
-                        ),
-                      );
-                      return;
-                    }
-
-                    if (newPassword != confirmPassword) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text(
-                              'New password and confirm password do not match'),
-                        ),
-                      );
-                      return;
-                    }
-
-                    // Mock Password Change Logic
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Password changed successfully!'),
-                      ),
-                    );
-
-                    // Clear Text Fields
-                    currentPasswordController.clear();
-                    newPasswordController.clear();
-                    confirmPasswordController.clear();
+                    setState(() {
+                      isCurrentPasswordVisible = !isCurrentPasswordVisible;
+                    });
                   },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFAF96F5), // Purple color
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    'Change Password',
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+
+            // New Password Input
+            TextField(
+              controller: newPasswordController,
+              obscureText: !isNewPasswordVisible,
+              decoration: InputDecoration(
+                labelText: 'New Password',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                prefixIcon: const Icon(Icons.lock_outline),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isNewPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isNewPasswordVisible = !isNewPasswordVisible;
+                    });
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // Confirm New Password Input
+            TextField(
+              controller: confirmPasswordController,
+              obscureText: !isConfirmPasswordVisible,
+              decoration: InputDecoration(
+                labelText: 'Confirm New Password',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                prefixIcon: const Icon(Icons.check_circle_outline),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isConfirmPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isConfirmPasswordVisible = !isConfirmPasswordVisible;
+                    });
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            // Change Password Button
+            ElevatedButton(
+              onPressed: () async {
+                final currentPassword = currentPasswordController.text;
+                final newPassword = newPasswordController.text;
+                final confirmPassword = confirmPasswordController.text;
+                final List<String> passwordErrors = [];
+
+                if (currentPassword.isEmpty ||
+                    newPassword.isEmpty ||
+                    confirmPassword.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Please fill in all fields'),
+                    ),
+                  );
+                  return;
+                }
+
+                if (newPassword != confirmPassword) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                          'New password and confirm password do not match'),
+                    ),
+                  );
+                  return;
+                }
+
+                if (newPassword.length < 12) {
+                  passwordErrors
+                      .add("Password must be at least 12 characters long.");
+                }
+                if (!RegExp(r'[A-Z]').hasMatch(newPassword)) {
+                  passwordErrors.add(
+                      "Password must contain at least one uppercase letter.");
+                }
+                if (!RegExp(r'[a-z]').hasMatch(newPassword)) {
+                  passwordErrors.add(
+                      "Password must contain at least one lowercase letter.");
+                }
+                if (!RegExp(r'[0-9]').hasMatch(newPassword)) {
+                  passwordErrors
+                      .add("Password must contain at least one number.");
+                }
+                if (!RegExp(r'[!@#\\$%^&*(),.?":{}|<>]')
+                    .hasMatch(newPassword)) {
+                  passwordErrors.add(
+                      "Password must contain at least one special character.");
+                }
+
+                if (passwordErrors.isNotEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(passwordErrors.join('\n')),
+                    ),
+                  );
+                  return;
+                }
+
+                try {
+                  User? user = auth.currentUser;
+
+                  AuthCredential credential = EmailAuthProvider.credential(
+                    email: user!.email!,
+                    password: currentPassword,
+                  );
+                  await user.reauthenticateWithCredential(credential);
+                  await user.updatePassword(newPassword);
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Password changed successfully!'),
+                    ),
+                  );
+
+                  currentPasswordController.clear();
+                  newPasswordController.clear();
+                  confirmPasswordController.clear();
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                          'Error: Please fill in the correct current password'),
+                    ),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF613EEA),
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Change Password',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
